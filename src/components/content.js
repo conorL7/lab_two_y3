b3e864534a9e438f2a61c40a6742ea7c1beb6c8c
Conor Lally <conorjbusiness@hotmail.com>
2021-10-19 10:45:18 +0100

    added accordion 1&2 - C.L

--- src/components/content.js
+++ src/components/content.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button, Card, Accordion } from 'react-bootstrap';
+
 
 
 class Content extends Component {
@@ -7,7 +9,26 @@ class Content extends Component {
             <div>
                 <h1>Hello World!</h1>
                 <h2>The time is {new Date().toLocaleTimeString()}.</h2>
+                <br></br>
+
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Accordion Item #1</Accordion.Header>
+                        <Accordion.Body>
+                            Accordion #1
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Accordion Item #2</Accordion.Header>
+                        <Accordion.Body>
+                            Accordion #2
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+
             </div>
+
+
         );
     }
 }
